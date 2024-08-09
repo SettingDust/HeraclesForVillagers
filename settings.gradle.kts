da125fa4aba6415777b937f5550ae006b89a363f
SettingDust <settingdust@gmail.com>
2024-08-09 12:18:21 +0800

    build: remove precommit git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -65,35 +65,6 @@ dependencyResolutionManagement.versionCatalogs.named("catalog") {
 plugins {
     // https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-    // https://github.com/DanySK/gradle-pre-commit-git-hooks
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.4"
-}
-
-gitHooks {
-    preCommit {
-        from {
-            //             git diff --cached --name-only --diff-filter=ACMR | while read -r a; do
-            // echo ${'$'}(readlink -f ${"$"}a); ./gradlew spotlessApply -q
-            // -PspotlessIdeHook="${'$'}(readlink -f ${"$"}a)" </dev/null; done
-            """
-            export JAVA_HOME="${System.getProperty("java.home")}"
-            ./gradlew spotlessApply spotlessCheck
-            """
-                .trimIndent()
-        }
-    }
-    commitMsg { conventionalCommits { defaultTypes() } }
-    hook("post-commit") {
-        from {
-            """
-            files="${'$'}(git show --pretty= --name-only | tr '\n' ' ')"
-            git add ${'$'}files
-            git -c core.hooksPath= commit --amend -C HEAD
-            """
-                .trimIndent()
-        }
-    }
-    createHooks(true)
 }
 
 val name: String by settings
